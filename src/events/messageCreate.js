79f11c8e69d22b11b5050c760d0f1439479c3215
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-20 22:38:09 +0000

    Give random amount of xp to successful bumper

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,14 @@
 const { Events } = require('discord.js');
 const { hyperBoardId, channelId } = require('../../config.json');
+const Level = require('../models/Level');
+const calculateLevelXp = require('../utils/calculateLevelXp');
+
+function getRandomXp(min, max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 module.exports = {
     name: Events.MessageCreate,
@@ -10,6 +19,8 @@ module.exports = {
         if (message.author.id === specificUserId && message.channel.id === specificChannelId) {
             const commandName = message.interaction?.commandName;
             const user = message.interaction?.user;
+            const userId = message.interaction?.user.id;
+            const userName = message.interaction?.user.username;
 
             if (!commandName || !user) {
                 console.error('Invalid interaction: missing commandName or user.');
@@ -17,7 +28,49 @@ module.exports = {
             }
 
             try {
-                if (commandName === "bump") {
+                if (commandName === "bump success") {
+
+                    const xpToGive = getRandomXp(5, 15);
+
+                    const query = {
+                        userId: userId,
+                        guildId: message.guild.id
+                    };
+
+                    const level = await Level.findOne(query);
+
+                    if (level) {
+                        console.log("Found user in database");
+                        level.xp += xpToGive;
+
+                        message.channel.send(`XP given to: ${userName}`);
+
+                        if (level.xp > calculateLevelXp(level.level)) {
+                            level.xp = 0;
+                            level.level += 1;
+
+                            message.channel.send(`${user} you have leveled up to **level ${level.level}**.`);
+                        }
+
+                        await level.save().catch((e) => {
+                            console.log(`Error saving updated level ${e}`);
+                            return
+                        });
+                    } else {
+                        console.log("Creating user in database");
+
+                        const newLevel = new Level({
+                            userId: userId,
+                            userName: userName,
+                            guildId: message.guild.id,
+                            xp: xpToGive,
+                        });
+
+                        await newLevel.save();
+                        message.channel.send(`XP given to: ${userName}`);
+
+                    }
+
                     console.log(`Bump detected from: ${user.tag}`);
                     await message.reply(`Detected a bump from: ${user}`);
                 } else {
